7c0ba3a0095f15a3d382b8787d4593a9880512c0
kia40star <kia40star@gmail.com>
2020-08-12 13:13:38 +0300

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+docker-compose build
+docker-compose up -d
+
+JSON_HEAD='content-type: application/json'
+
+BASEURL='http://127.0.0.1:8000'
+URLS="$BASEURL/urls"
+KEYS="$BASEURL/keys"
+
+FILENAME_RANGE=$(seq 1 2)
+
+ATTEMPT_TIMEOUT_SEC=1
+MAX_ATTEMPTS=5
+
+# In general, IDS is not subset of FILENAME_RANGE
+for i in $FILENAME_RANGE; do
+    IDS[$i]=$(curl -X POST -H $JSON_HEAD $URLS \
+        -d '{
+            "url": "https://hub.b2basket.ru/media/test_problem/file_'$i'.xml"
+        }' | jq '.id')
+done
+
+ATTEMPT=0
+
+while [[ "$READY" != 'true' ]]; do
+    ATTEMPT=$((ATTEMPT+1))
+
+    if [[ $ATTEMPT > $MAX_ATTEMPTS ]]; do
+        echo "\e[0;31mToo many attempts!\e[m"
+        curl -H $JSON_HEAD $URLS
+        exit
+    done
+
+    READY='true'
+
+    for i in $IDS[*]; do
+        PROCESSED=$(curl -H $JSON_HEAD "$URLS/$i" | jq '.processed')
+
+        if [[ "$PROCESSED" != 'true' ]]; then
+            READY='false'
+            sleep $ATTEMPT_TIMEOUT_SEC
+            break
+        fi
+done
+
+curl -H $JSON_HEAD $KEYS
